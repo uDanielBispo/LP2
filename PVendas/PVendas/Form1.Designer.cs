6ab06d8235db7c2ce26e6027344473df4b353c29
uDanielBispo <danielbispo3015@hotmail.com>
2022-05-21 11:25:58 -0300

    Adicionando prova do dia 20/05, essa versao nao e .zip

---
+++ PVendas/PVendas/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ProvaPratica1
+{
+    partial class frmVendas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxVendas = new System.Windows.Forms.ListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbxVendas
+            // 
+            this.lstbxVendas.FormattingEnabled = true;
+            this.lstbxVendas.Location = new System.Drawing.Point(38, 26);
+            this.lstbxVendas.Name = "lstbxVendas";
+            this.lstbxVendas.Size = new System.Drawing.Size(571, 394);
+            this.lstbxVendas.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(646, 26);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(123, 394);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // frmVendas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lstbxVendas);
+            this.Name = "frmVendas";
+            this.Text = "Vendas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxVendas;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ PVendas/PVendas/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ProvaPratica1
+{
+    public partial class frmVendas : Form
+    {
+        public frmVendas()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            lstbxVendas.Items.Clear();
+
+            int n = 5;
+            double[,] vendas = new double[n, 4];
+            double[] vSemana = new double[4];
+            double[] totMes = new double[n];
+            double auxTotMes = 0;
+            double totGeral = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    string aux = Interaction.InputBox($"Digite as vendas da {j+1}ª semana do mes {i+1}", "Entrada de dados");
+                    if (aux == String.Empty)
+                    {
+                        lstbxVendas.Items.Add($">> Total Geral: R$" + totGeral.ToString("N2"));
+                        return;
+                    }
+
+                    else if (!Double.TryParse(aux, out vSemana[j]))
+                    {
+                        MessageBox.Show("Valor inválido");
+                        j--;
+                    }
+                    else
+                    {
+                        lstbxVendas.Items.Add($"Total do mês {i + 1} semana {j + 1}: R$" + vSemana[j].ToString("N2"));
+
+                        auxTotMes += vSemana[j];
+                        totGeral += vSemana[j];
+                    }                    
+                }
+                totMes[i] = auxTotMes;              
+                auxTotMes = 0;
+
+                lstbxVendas.Items.Add($">> Total mês: R$"+ totMes[i].ToString("N2"));
+                lstbxVendas.Items.Add("-----------------------------");
+            }
+            lstbxVendas.Items.Add($">> Total Geral: R$" + totGeral.ToString("N2"));
+        }
+    }
+}
